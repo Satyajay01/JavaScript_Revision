4e44c24e09425f9483862a03c9ecde7d5b90459b
satyajay01 <satyajaydibya0@gmail.com>
2024-11-19 15:05:21 +0530

    Variables

---
+++ 01_Variables/Variables.js
@@ -0,0 +1,25 @@
+const AC_number= 123456789;
+let Email_id = "satyajaydibya9@gmail.com";
+var password = "Satyajay@123";
+City = "Raipur";
+
+// console.log(AC_number);
+// console.log(Email_id);
+// console.log(password);
+// console.log(City+" DataType= "+typeof(City));
+
+
+// *************Upadte*************
+
+// AC_number = 987654321; The const variable is not changeable.
+Email_id = "satyajay@gmail.com";
+password = "Saty@123";
+City = "Bilaspur";
+
+// console.log(AC_number);
+console.log(Email_id);
+console.log(password);
+console.log(City);
+
+console.table([AC_number,Email_id,password,City]);
+
