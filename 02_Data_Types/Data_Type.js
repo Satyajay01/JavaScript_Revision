1345bd8573639a4997c92e0078a7f2ceba369e73
satyajay01 <satyajaydibya0@gmail.com>
2024-11-19 16:01:37 +0530

    Data Types

---
+++ 02_Data_Types/Data_Type.js
@@ -0,0 +1,33 @@
+
+// Number
+let num = 123456789;
+console.log(typeof(num));  
+
+// string
+let name = "Satyajay";
+console.log(typeof(name));
+
+// boolean
+let x = true;
+console.log(typeof(x)); 
+let y = false; 
+console.log(typeof(y));
+
+// object
+let person = {name: "Satyajay", age:23};
+console.log(typeof(person));
+
+// Array
+let sabji = ["kundaru", "karela", "murai"];
+console.log(Array.isArray(sabji));
+
+// ************
+
+// date
+const date = new Date("2022-03-25");
+// null
+let outsideTemp = null; 
+// undefined
+let userEmail; 
+// Symbol
+const id = Symbol('123'); 
